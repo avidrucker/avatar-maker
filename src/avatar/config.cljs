a35e965ac6ab32a091fdbe7f5628ce30e4abf78a
avidrucker <avidrucker@gmail.com>
2026-02-21 09:21:50 -1000

    add geometry constants, rename to geometry for clarity

--- src/avatar/config.cljs
+++ src/avatar/config.cljs
@@ -1,9 +1,34 @@
 (ns avatar.config)
 
-(def constants
-  {:head/cx 256 :head/cy 270 :head/scale 6.0 :head/y-offset 0
-   :eyes/spacing {:min 0.2 :max 1.0 :step 0.01}
-   ;; ... add other constants here
+(def geometry
+  {:head/cx 256
+   :head/cy 270
+   :head/scale 6.0
+   :head/y-offset 0
+
+   :nose/base-y 320
+   :nose/scale 1.25
+   :nose/stroke-width 2
+
+   :eyes/base-y 260
+   :eyes/dx-scale 110
+   :eyes/rotate-left 0
+   :eyes/rotate-right 0
+
+   :brows/base-y 225
+   :brows/base-dx 70
+
+   :mouth/base-y 365
+   :mouth/scale 1.2
+
+   :glasses/base-y 280
+   :glasses/scale 3.0
+
+   :ears/head-rx 160
+   :ears/distance-padding 10
+   :ears/base-y-offset -30
+   :ears/rotate-left -15
+   :ears/rotate-right 15
    })
 
 (def skin-tones
