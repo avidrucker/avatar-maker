defa09017d1d5a505999fd53d9baa7a4e1a672e1
avidrucker <avidrucker@gmail.com>
2026-02-22 09:23:01 -1000

    add brows, clamping, svg rendering

--- src/avatar/render.cljs
+++ src/avatar/render.cljs
@@ -115,6 +115,59 @@
   (make-hair {}))
 
 ;; -------------------------
+;; Brow renderers
+;; -------------------------
+
+(defn brow-root
+  "Center a normalized 100x100 brow asset at (0,0)."
+  [& children]
+  (into
+   [:g {:transform "translate(-50 -50)"}]
+   (with-keys children)))
+
+(defn make-brow
+  "Create a brow renderer from one normalized SVG element."
+  [element]
+  (fn brow-renderer [{:keys [color]}]
+    (brow-root
+     (let [[tag attrs] element]
+       [tag (assoc attrs :fill (or color "black"))]))))
+
+(defn brow-none [_] nil)
+
+(def brow-001
+  (make-brow
+   [:path {:d "M62 27.5L6.5 56.5V66.5L80.5 45L62 27.5Z"}]))
+
+(def brow-002
+  (make-brow
+   [:path {:d "M8.50037 56.5C42.0398 28.635 57.5429 28.4721 81.5004 45.5C82.5 48.5 80.5 50 78.0004 49C59.83 38.4599 47.5507 36.6841 12.5004 59.5C9.50032 60.5 8.00003 59.5 8.50037 56.5Z"}]))
+
+(def brow-003
+  (make-brow
+   [:path {:d "M55.5 32.5L5.5 48L9 66C57 52 58 51.5 82 57.5L55.5 32.5Z"}]))
+
+(def brow-004
+  (make-brow
+   [:path {:d "M11.0914 54.0113C43.5529 29.3355 58.5578 29.1912 81.7453 44.2703C82.7128 46.9269 80.777 48.2552 78.3578 47.3697C57.0956 37.2186 48.7578 42.1117 21.5 62.5C13.6126 69.0701 5.13936 62.903 11.0914 54.0113Z"}]))
+
+(def brow-005
+  (make-brow
+   [:path {:d "M79 43C49 41.6036 39.9558 45.8628 29 64.0001C22 71.5 9.5 68 9.5 55.0001C27 27 55.5 24.9999 79 43Z"}]))
+
+(def brow-006
+  (make-brow
+   [:rect {:x 11 :y 44 :width 63 :height 17}]))
+
+(def brow-007
+  (make-brow
+   [:path {:d "M8 43.4999C34.6082 43.1703 46.492 43.3306 55.5 44.9999C64.8392 48.4078 69.6547 51.8843 78 58.9999L73.5 66.9999C65.5721 62.596 61.275 60.3375 55.5 58.9999C37.5 57.4999 26.7077 61.4164 8 59.9999V43.4999Z"}]))
+
+(def brow-008
+  (make-brow
+   [:path {:d "M44 44.4999C57.5 35 73.5 39.5 83 51.4999C68.388 46.1004 60.2547 43.7399 47 53.4999C26 65.5 11 63 9 57.9999C10.5 47.5 23 57.9999 44 44.4999Z"}]))
+
+;; -------------------------
 ;; Feature registry
 ;; -------------------------
 
@@ -136,7 +189,18 @@
    :nose {:default :none :shapes {:none {:label "None" :render (fn [_] nil) :order 0}}}
    :ears {:default :none :shapes {:none {:label "None" :render (fn [_] nil) :order 0}}}
    :mouth {:default :none :shapes {:none {:label "None" :render (fn [_] nil) :order 0}}}
-   :brows {:default :none :shapes {:none {:label "None" :render (fn [_] nil) :order 0}}}})
+   :brows
+   {:default :none
+    :shapes
+    {:none {:label "None" :render brow-none :order 0}
+     :001 {:label "001" :render brow-001 :order 1}
+     :002 {:label "002" :render brow-002 :order 2}
+     :003 {:label "003" :render brow-003 :order 3}
+     :004 {:label "004" :render brow-004 :order 4}
+     :005 {:label "005" :render brow-005 :order 5}
+     :006 {:label "006" :render brow-006 :order 6}
+     :007 {:label "007" :render brow-007 :order 7}
+     :008 {:label "008" :render brow-008 :order 8}}}})
 
 (defn sorted-shape-entries [feature]
   (->> (get-in feature-registry [feature :shapes])
@@ -200,6 +264,17 @@
                     :jet-black))
       (update-in [:parts :brows] #(or % {:shape :none :size 1.0 :x-offset 0 :y-offset 0 :rotation 0}))))
 
+(defn clamp
+  [mn mx v]
+  (-> v (max mn) (min mx)))
+
+(defn clamp-cfg
+  [k value]
+  (let [{:keys [min max]} (get cfg/constants k)]
+    (if (and (number? min) (number? max) (number? value))
+      (clamp min max value)
+      value)))
+
 ;; -------------------------
 ;; Avatar render pipeline
 ;; -------------------------
@@ -237,7 +312,30 @@
 (defn mouth-svg [_mouth] nil)
 (defn nose-svg [_nose _head] nil)
 (defn eyes-svg [_eyes] nil)
-(defn brows-svg [_brows _hair] nil)
+(defn brows-svg
+  [{:keys [shape size x-offset y-offset rotation]} hair]
+  (when (not= shape :none)
+    (let [sc (clamp-cfg :brows/size (or size 1.0))
+          xoff (clamp-cfg :brows/x-offset (or x-offset 0))
+          yoff (clamp-cfg :brows/y-offset (or y-offset 0))
+          rot (clamp-cfg :brows/rotation (or rotation 0))
+          head-cx (:head/cx cfg/geometry)
+          cy (+ (:brows/base-y cfg/geometry) yoff)
+          dx (+ (:brows/base-dx cfg/geometry) xoff)
+          brow-fn (resolve-renderer :brows shape)
+          brow-color (get cfg/hair-colors (:color hair) (get cfg/hair-colors :jet-black))]
+      [:g
+       [:g {:transform
+            (str "translate(" (- head-cx dx) " " cy ") "
+                 "rotate(" (* -1 rot) ") "
+                 "scale(" sc ") "
+                 "scale(-1 1)")}
+        (brow-fn {:color brow-color})]
+       [:g {:transform
+            (str "translate(" (+ head-cx dx) " " cy ") "
+                 "rotate(" rot ") "
+                 "scale(" sc ")")}
+        (brow-fn {:color brow-color})]])))
 (defn glasses-svg [_glasses] nil)
 
 (defn avatar->hiccup
