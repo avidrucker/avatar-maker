447e360fb623ef0b3f65ccc7b8423ea278f7682a
avidrucker <avidrucker@gmail.com>
2026-02-26 08:12:33 -1000

    feat modify title in localhost only to indicate dev env

--- src/avatar/core.cljs
+++ src/avatar/core.cljs
@@ -7,13 +7,26 @@
 (defonce root
   (rdomc/create-root (.getElementById js/document "app")))
 
+(defn localhost?
+  []
+  (contains? #{"localhost" "127.0.0.1" "::1" "[::1]"}
+             (.-hostname js/location)))
+
+(defn set-page-title! []
+  (set! (.-title js/document)
+        (if (localhost?)
+          "Avatar Maker (Dev)"
+          "Avatar Maker")))
+
 (defn render! []
   (rdomc/render root [ui/main-panel]))
 
 (defn init! []
   ;; Load from localStorage and initialize persistence watchers.
+  (set-page-title!)
   (storage/init!)
   (render!))
 
 (defn ^:dev/after-load rerender []
+  (set-page-title!)
   (render!))
